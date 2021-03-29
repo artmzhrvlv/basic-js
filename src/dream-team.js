bc41cba9cba69c1e4e0eb445a3c2364dd8aaa711
artmzhrvlv <61042904+artmzhrvlv@users.noreply.github.com>
2021-03-30 00:28:03 +0300

    feat: start task basic-js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+    if (!Array.isArray(arr)) {
+      return false
+    }
+    else {
+      n = [];
+      if (arr === []) return arr;
+      else {
+        arr.forEach(function(item) {
+          if (typeof(item) == 'string') {
+            str = item.trim().toUpperCase();
+            n.push(str[0]);
+          }
+        });
+        if ( n != []) return n.sort().join('');
+        else return n;
+      }
+    }
 };
