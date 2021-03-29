7187aaafcc6904351a94efa721c285b9c7c83563
artmzhrvlv <61042904+artmzhrvlv@users.noreply.github.com>
2021-03-30 00:33:50 +0300

    feat: start transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,34 @@
 const CustomError = require("../extensions/custom-error");
-
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(matrix) {
+    transformed = [];
+    if (!Array.isArray(matrix)) throw new Error();
+    for (let i = 0; i < matrix.length; i++) {
+        switch (matrix[i]) {
+          case "--double-next":  
+            { if (i != matrix.length - 1) {
+              transformed.push(matrix[i + 1]);
+              }
+              break;
+            }
+          case "--double-prev":
+            {  
+              if (matrix[i - 2] != "--discard-next" && i != 0)  transformed.push(matrix[i - 1]);
+              break;
+            }
+          case "--discard-prev":
+            {  
+              if (matrix[i - 2] != "--discard-next" && i != 0) transformed.pop(); 
+              break;
+            }  
+          case "--discard-next":
+            {
+              if (i != matrix.length - 1) i++;
+              break;
+            }
+          default: { 
+            transformed.push(matrix[i]);
+          }      
+      }
+    }
+    return transformed;
 };
