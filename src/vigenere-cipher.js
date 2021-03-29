3ad3a3a37f4b65546d59f154ae47ea049107fcef
artmzhrvlv <61042904+artmzhrvlv@users.noreply.github.com>
2021-03-30 00:35:31 +0300

    feat: start vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,61 @@
 const CustomError = require("../extensions/custom-error");
-
+const dictString = "abcdefghijklmnopqrstuvwxyz";
+const dictArray = dictString.split('');
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct) {
+    if (direct === undefined || direct === true) direct = true;
+    this.directSeq = direct;
+  }
+  encrypt(message, key) {
+    let newKey = '', solutionStr = '', sum = 0, j = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      if (dictArray.indexOf((message[i].toLowerCase())) === -1 || message[i] === ' ') newKey += message[i];
+      else {
+        newKey += key[j].toLowerCase();
+        j++;
+        if (j === key.length) j = 0;
+      }
+    }
+    for (let i = 0; i < message.length; i++) {
+      if (dictArray.indexOf((message[i].toLowerCase())) === -1 || message[i] === ' ') solutionStr += message[i];
+      else {
+        if (dictArray.indexOf(newKey[i]) + dictArray.indexOf(message[i].toLowerCase()) >= 26){
+
+          sum = dictArray.indexOf(newKey[i]) + dictArray.indexOf(message[i].toLowerCase()) - 26;
+        }
+        else {
+          sum = dictArray.indexOf(newKey[i]) + dictArray.indexOf(message[i].toLowerCase());
+        }
+        solutionStr += dictArray[sum];
+      } 
+    }
+    return (this.directSeq) ? solutionStr.toUpperCase() : solutionStr.split('').reverse().join('').toUpperCase();
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(message, key) {
+    let newKey = '', solutionStr = '', sum = 0, j = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      if (dictArray.indexOf((message[i].toLowerCase())) === -1 || message[i] === ' ') newKey += message[i];
+      else {
+        newKey += key[j].toLowerCase();
+        j++;
+        if (j === key.length) j = 0;
+      }
+    }
+    for (let i = 0; i < message.length; i++) {
+      if (dictArray.indexOf((message[i].toLowerCase())) === -1 || message[i] === ' ') solutionStr += message[i];
+      else {
+        if (dictArray.indexOf(message[i].toLowerCase()) - dictArray.indexOf(newKey[i]) < 0) {
+          sum = dictArray.indexOf(message[i].toLowerCase()) - dictArray.indexOf(newKey[i]) + 26;
+        }
+        else {
+          sum = dictArray.indexOf(message[i].toLowerCase()) - dictArray.indexOf(newKey[i]);
+        }   
+        solutionStr += dictArray[sum];
+      }
+    }   
+    return (this.directSeq) ? solutionStr.toUpperCase() : solutionStr.split('').reverse().join('').toUpperCase();
   }
 }
-
 module.exports = VigenereCipheringMachine;
